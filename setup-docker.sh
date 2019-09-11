0f0ff0e537230cc61c24dd294fca696c7c6b4f83
Miguel Gall Martin <migall_us@MiguelORACLEMac.attlocal.net>
2019-09-11 09:02:47 -0400

    MGM including gallmiquel on the docker references

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # build the flask container
-docker build -t prakhar1989/foodtrucks-web .
+docker build -t gallmiquel/foodtrucks-web .
 
 # create the network
 docker network create foodtrucks-net
@@ -10,4 +10,4 @@ docker network create foodtrucks-net
 docker run -d --name es --net foodtrucks-net -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
 
 # start the flask app container
-docker run -d --net foodtrucks-net -p 5000:5000 --name foodtrucks-web prakhar1989/foodtrucks-web
+docker run -d --net foodtrucks-net -p 5000:5000 --name foodtrucks-web gallmiquel/foodtrucks-web
